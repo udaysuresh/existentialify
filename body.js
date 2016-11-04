dbaeb4159899e01997d60c566569cb8f9e6b0f77
Uday Suresh <udaysuresh@berkeley.edu>
2016-11-03 23:47:22 -0700

    Add files via upload

---
+++ body.js
@@ -0,0 +1,43 @@
+walk(document.body);
+
+function walk(node)  
+{
+    // More than inspired by Steven Frank 
+    // this function was borrowed from him
+    
+    var child, next;
+
+    switch ( node.nodeType )  
+    {
+        case 1: 
+        case 9:  
+        case 11: 
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling; 
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: 
+            handleText(node);
+            break;
+    }
+}
+
+function handleText(textNode) 
+{
+    var v = textNode.nodeValue;
+
+    v = v.replace(/\bHappiness\b/g, "Lies");
+    v = v.replace(/\bhappiness\b/g, "lies");
+    v = v.replace(/\bHappy\b/g, "Deceived");
+    v = v.replace(/\bhappy\b/g, "deceived");
+    v = v.replace(/\bPermanent\b/g, "Ephemeral");
+    v = v.replace(/\bpermanent\b/g, "ephemeral");
+    //reminder to improve js skills
+    
+    textNode.nodeValue = v;
+}
